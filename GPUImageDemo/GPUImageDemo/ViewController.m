72fabbb1fb833b3be26f7f7dd5f015d048512adb
yuanyuntian <yuanf1990@163.com>
2021-03-15 22:07:25 +0800

    接入GPUImage框架

--- GPUImageDemo/GPUImageDemo/ViewController.m
+++ GPUImageDemo/GPUImageDemo/ViewController.m
@@ -8,6 +8,7 @@
 #import "ViewController.h"
 #import "PTImageEditViewController.h"
 
+
 @interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
 
 @property(nonatomic, weak)IBOutlet UITableView * tableView;
--- GPUImageDemo/GPUImageDemo/图片处理/PTImageEditViewController.m
+++ GPUImageDemo/GPUImageDemo/图片处理/PTImageEditViewController.m
@@ -7,10 +7,16 @@
 
 #import "PTImageEditViewController.h"
 #import <TZImagePickerController/TZImagePickerController.h>
+#import <TYCyclePagerView/TYCyclePagerView-umbrella.h>
+#import <Masonry/Masonry.h>
+#import "TYCyclePagerViewCell.h"
+#import <GPUImage/GPUImage.h>
 
-@interface PTImageEditViewController ()<TZImagePickerControllerDelegate>
+@interface PTImageEditViewController ()<TZImagePickerControllerDelegate,TYCyclePagerViewDataSource,TYCyclePagerViewDelegate>
 @property(nonatomic, weak)IBOutlet UIButton * add;
 @property(nonatomic, weak)IBOutlet UIImageView * imageView;
+@property(nonatomic, strong) TYCyclePagerView * menu;
+@property(nonatomic, strong) NSArray * items;
 
 @end
 
@@ -19,8 +25,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.view addSubview:self.menu];
+    self.items = @[@"1",@"1",@"1",@"1"];
+    [self.menu reloadData];
 }
 
+
+-(TYCyclePagerView*)menu {
+    if (_menu == nil) {
+        _menu = [TYCyclePagerView new];
+        _menu.isInfiniteLoop = false;
+        _menu.autoScrollInterval = 0;
+        _menu.delegate = self;
+        _menu.dataSource = self;
+        [_menu registerClass:[TYCyclePagerViewCell class] forCellWithReuseIdentifier:@"cellId"];
+    }
+    return _menu;
+}
+
+-(void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    [self.menu mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.view);
+        if (@available(iOS 11.0,*)){
+            make.bottom.equalTo(self.view.mas_safeAreaLayoutGuideBottom);
+        }else{
+            make.bottom.equalTo(self.mas_bottomLayoutGuide);
+        }
+        make.height.mas_equalTo(80);
+    }];
+}
 
 -(IBAction)addImageAction:(id)sender {
     TZImagePickerController *imagePickerVc = [[TZImagePickerController alloc] initWithMaxImagesCount:0 columnNumber:4 delegate:self pushPhotoPickerVc:true];
@@ -38,21 +73,52 @@
     [self presentViewController:imagePickerVc animated:YES completion:nil];
     
     [imagePickerVc setDidFinishPickingPhotosHandle:^(NSArray<UIImage *> *photos, NSArray *assets, BOOL isSelectOriginalPhoto) {
-        
+        self.imageView.image = photos.firstObject;
+        self.add.hidden = true;
     }];
 }
 
 
 
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (nonnull TYCyclePagerViewLayout *)layoutForPagerView:(nonnull TYCyclePagerView *)pageView {
+    TYCyclePagerViewLayout *layout = [[TYCyclePagerViewLayout alloc]init];
+    layout.itemSize = CGSizeMake(120, 80);
+    layout.itemSpacing = 15;
+    //layout.minimumAlpha = 0.3;
+    return layout;
 }
-*/
+
+- (NSInteger)numberOfItemsInPagerView:(nonnull TYCyclePagerView *)pageView {
+    return self.items.count;
+}
+
+- (nonnull __kindof UICollectionViewCell *)pagerView:(nonnull TYCyclePagerView *)pagerView cellForItemAtIndex:(NSInteger)index {
+    TYCyclePagerViewCell *cell = [pagerView dequeueReusableCellWithReuseIdentifier:@"cellId" forIndex:index];
+    cell.backgroundColor = UIColor.redColor;
+    cell.label.text =self.items[index];
+    return cell;
+}
+
+
+-(UIImage*)hightLightImage:(UIImage*)image withType:(NSInteger)type {
+    switch (type) {
+        case 0:{
+            //中间突出  四周暗
+            GPUImageVignetteFilter *filter = [GPUImageVignetteFilter new];
+            [filter forceProcessingAtSize:image.size];
+            [filter useNextFrameForImageCapture];
+            
+            GPUImagePicture * stillImageSource = [[GPUImagePicture alloc] initWithImage:image];
+        }
+        break;
+        default:
+            break;
+    }
+    return nil;
+}
+
+
 
 @end
